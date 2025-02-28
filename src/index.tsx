ec169983a517da70825ce810904a7b81d2a32c8e
Joshua <joshua.w.hoy@gmail.com>
2025-02-28 23:11:40 +0000

    Small update

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+export type { IconProps } from './types';
+export { IconFamily } from './IconFamily';
+export type { IconFamilyProps } from './IconFamily';
+export type { IconFamilyConfig } from './IconFamilyContext';
+
+export { default as Bin } from './icons/BinIcon';
